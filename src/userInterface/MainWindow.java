4543ff45f31f4ee47ba5b044b6faccac43242e17
Mathias <mathiasvandercuylen@gmail.com>
2025-04-11 10:56:54 +0200

    Ajout MainWindow

--- src/userInterface/MainWindow.java
+++ src/userInterface/MainWindow.java
@@ -1,4 +1,17 @@
 package userInterface;
 
-public class MainWindow {
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow(){
+        super("Accueil");
+        setBounds(0,0,1280,720);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+    }
 }
