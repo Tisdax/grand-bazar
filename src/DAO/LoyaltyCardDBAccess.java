825fc63652a9f5eeedf5ea6cf9dd62ffb0de8dbd
Mathias <mathiasvandercuylen@gmail.com>
2025-05-22 14:24:22 +0200

    tsk tsk

--- src/DAO/LoyaltyCardDBAccess.java
+++ src/DAO/LoyaltyCardDBAccess.java
@@ -5,6 +5,7 @@ import exceptions.DAOException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoyaltyCardDBAccess implements LoyaltyCardDAO {
@@ -22,5 +23,32 @@ public class LoyaltyCardDBAccess implements LoyaltyCardDAO {
             throw new DAOException(e.getMessage(), "Erreur lors de la suppression de la carte de fidélité");
         }
     }
+    public int lastId() throws DAOException {
+        String sqlInstruction = "select max(number) from loyalty_card";
+        try {
+            Connection connection = SingletonConnection.getInstance();
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlInstruction);
 
+            ResultSet data = preparedStatement.executeQuery();
+
+            return data.next() ? data.getInt(1) : 0;
+        } catch (SQLException e) {
+            throw new DAOException(e.getMessage(), "Problème lors de la recheche du dernier identifiant");
+        }
+    }
+    public void addLoyaltyCard(int customerId) throws DAOException {
+        String sqlInstruction = "insert into loyalty_card(number, total_points, is_valid, customer) values (?, 0, true, ?)";
+        try {
+            Connection connection = SingletonConnection.getInstance();
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlInstruction);
+
+            preparedStatement.setInt(1, lastId()+1);
+            preparedStatement.setInt(2, customerId);
+
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new DAOException(e.getMessage(), "Erreur lors de l'ajout de carte de fidélité");
+        }
+    }
 }
--- src/DAOinterfaces/LoyaltyCardDAO.java
+++ src/DAOinterfaces/LoyaltyCardDAO.java
@@ -4,4 +4,6 @@ import exceptions.DAOException;
 
 public interface LoyaltyCardDAO {
     public int delete(int loyaltyCardId) throws DAOException;
+
+    void addLoyaltyCard(int customerId);
 }
