17d07da405bd793a86e07a6847448cd39b1ed500
Locht <etu54287@henallux.be>
2025-05-23 22:19:35 +0200

    ajout méthode manquante LoyaltyCardDAO

--- src/DAOinterfaces/LoyaltyCardDAO.java
+++ src/DAOinterfaces/LoyaltyCardDAO.java
@@ -4,5 +4,6 @@ import exceptions.DAOException;
 
 public interface LoyaltyCardDAO {
     public int delete(int loyaltyCardId) throws DAOException;
+    public int lastId() throws DAOException;
     public void save(int customerId) throws DAOException;
 }
