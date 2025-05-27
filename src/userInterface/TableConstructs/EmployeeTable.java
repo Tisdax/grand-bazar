5b1a700c44a082ad2867addbef1d83b5ac01b834
carto <cdt.martin@icloud.com>
2025-05-27 17:59:00 +0200

    C'estBOOOOOOOOOON

--- src/userInterface/TableConstructs/EmployeeTable.java
+++ src/userInterface/TableConstructs/EmployeeTable.java
@@ -1,7 +1,11 @@
 package userInterface.TableConstructs;
 
+import exceptions.DAOException;
+import exceptions.InvalidValueException;
 import model.Employee;
+import model.Product;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class EmployeeTable extends TableConstruct {
@@ -11,7 +15,15 @@ public class EmployeeTable extends TableConstruct {
     }
     public void fillTable() {
         try {
-            ArrayList<Employee> employees = controller.
+            ArrayList<Employee> employees = controller.findAllEmployee();
+            super.refreshTable();
+            for(Employee employee : employees) {
+                tableModel.addRow(new Object[] {
+                        employee.getId(), employee.getFirstName(), employee.getLastName()
+                });
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
