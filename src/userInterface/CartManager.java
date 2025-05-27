44633466a5a61ac7cac491276e919dda7081d591
carto <cdt.martin@icloud.com>
2025-05-27 17:41:29 +0200

    Add Emplyee Manager

--- src/userInterface/CartManager.java
+++ src/userInterface/CartManager.java
@@ -1,6 +1,7 @@
 package userInterface;
 
 import userInterface.TableConstructs.CustomerTable;
+import userInterface.TableConstructs.EmployeeTable;
 import userInterface.TableConstructs.ProductTable;
 import userInterface.TableConstructs.TableConstruct;
 
@@ -10,9 +11,10 @@ import java.awt.*;
 
 public class CartManager extends JPanel {
     private JLabel titleLabel;
-    private TableConstruct table;
     private JButton selectEmployee;
     private JButton selectCustomer;
+    private CustomerTable customerTable;
+    private EmployeeTable employeeTable;
 
 
     public CartManager() {
@@ -24,14 +26,21 @@ public class CartManager extends JPanel {
         titleLabel = new JLabel("Gestion Achats");
         titleLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
         titlePanel.add(titleLabel);
+
         // Buttons
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        // Tables
+        customerTable = new CustomerTable();
+        JScrollPane customerScrollTable = new JScrollPane(customerTable.getTable());
 
         selectEmployee = new JButton("Sélectionner un employé");
         selectEmployee.setPreferredSize(new Dimension(350, 80));
         buttonPanel.add(selectEmployee);
         selectEmployee.addActionListener(e -> {
-
+            this.remove(customerScrollTable);
+            this.revalidate();
+            this.repaint();
         });
 
         selectCustomer = new JButton("Sélectionner un client");
@@ -41,12 +50,10 @@ public class CartManager extends JPanel {
         });
 
 
-        table = new CustomerTable();
-
 
         this.add(titlePanel, BorderLayout.NORTH);
         this.add(buttonPanel, BorderLayout.CENTER);
-        this.add(new JScrollPane(table.getTable()), BorderLayout.SOUTH);
+        this.add(customerScrollTable, BorderLayout.SOUTH);
     }
 }
 
